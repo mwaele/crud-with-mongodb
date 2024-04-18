7381d9617fe0ecb9d55ff17bfc8e8f04875b2483
TUM-HIS <tum.his.club@gmail.com>
2024-04-18 13:54:32 +0300

    Imported mongoose package, Connected to mongoDB database

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express")
 const app = express()
 
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to MongoDB");
+        }
+    }
+)
+
 //middleware
 app.use(express.json());
 
